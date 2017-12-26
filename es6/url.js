42909caa09e4fa637d0835f9bcc77af59c2899cf
e <1127431465@qq.com>
2017-12-26 23:31:18 +0800

    判断上网用的设备并解析url的对象

---
+++ es6/url.js
@@ -0,0 +1,18 @@
+const url = 'http://localhost:3000/ecdo.html?a=1&b=3'
+// 查询参数部分
+let query_string = url.substring(
+    url.indexOf('?')+1
+);
+console.log(query_string);
+
+let qsArr = query_string.split('&');
+console.log(qsArr);
+
+const qsObj = {};
+for(let qs_item of qsArr){
+    let pos = qs_item.indexOf('=');
+    let key = qs_item.substring(0,pos);
+    let value = qs_item.substring(pos+1);
+    qsObj[key] = value;
+}
+console.log(qsObj);
